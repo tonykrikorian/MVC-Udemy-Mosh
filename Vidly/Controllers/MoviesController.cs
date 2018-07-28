8a287f8f2da2032a9efe00981970bc7eb5ffc13c
Tony <tony.krikorian@outlook.com>
2018-07-28 11:30:51 -0400

    Cambios de MVC Getting Started Curso Mosh UDemy

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name ="Shrek"};
+            //return View(movie);
+            //return Content("Hello World", "XXX");
+            //return HttpNotFound("No se puede encontrar el recurso");
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, orderBy = "name" });
+        }
+    }
+}
